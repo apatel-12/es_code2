74816953ddab429062d4938086142ad27f1f1aec
Dev user <dev.user@test.com>
2021-05-17 08:05:24 -0400

    Index update

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 <title>Employee Manager</title>
-<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="frontend/css/app.css">
 </head>
 <body>
     <h1>Demo</h1>
     <ul>
-        <li><a href="/frontend/login.html">Login</a></li>
-        <li><a href="/frontend/employee_edit.html">Edit Profile</a></li>
+        <li><a href="frontend/login.html">Login</a></li>
+        
     </ul>
 </body>
